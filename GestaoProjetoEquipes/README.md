9795c80905cbb5fccc6cfd12fc3ab2bf6b033e77
lucas araujo <70071400+lucasaraujoXD@users.noreply.github.com>
2026-03-25 10:48:07 -0300

    first commit

---
+++ GestaoProjetoEquipes/README.md
@@ -0,0 +1,1 @@
+O Projeto envolve a construção de um Sistema para Gestão de Projetos e também das Equipes, garantindo o cadastro e manutenção de usuários do sistema; de projetos; de tarefas do projeto; de equipes; de alocação das equipes nos projetos; de relatórios de acompanhamento dos projetos.
---
+++ GestaoProjetoEquipes/src/App.java
@@ -0,0 +1,53 @@
+import javax.swing.JOptionPane;
+
+public class App {
+   
+
+    public static void main(String[] args) {
+        
+        String primeiroNome = "Lucas";
+        String ultimoNome = "Araujo";
+        
+        String nomeCompleto = primeiroNome + " " + ultimoNome;
+        
+        System.out.println(primeiroNome);
+        System.out.println(ultimoNome);
+        System.out.println(nomeCompleto);
+        
+        int numero1 = 2;
+        int numero2 = 3;
+        
+        int resultadoSoma = numero1 + numero2;
+        
+        System.out.println(resultadoSoma);
+        
+        String numero3 = "2";
+        String numero4 = "3";
+        
+        String resultadoJuncao = numero3 + numero4;
+        
+        System.out.println(resultadoJuncao);
+        
+        double a = 1;
+        double b = 2;
+        
+        double soma = a + b;
+        double subtracao = a - b;
+        double multiplicacao = a * b;
+        double divisao = a / b;
+        
+        System.out.println("soma: " + soma);
+        System.out.println("subtração: " + subtracao);
+        System.out.println("multiplicação: " + multiplicacao);
+        System.out.println("divisão: " + divisao);
+        
+        String firstName = JOptionPane.showInputDialog("Digite seu primeiro nome: ");
+        String lastName = JOptionPane.showInputDialog("Digite seu último nome: ");
+        String idade = JOptionPane.showInputDialog("Qual a sua idade: ");
+        
+        String cliente = firstName + " " + lastName + " com idade de " + idade + " anos";
+        
+        JOptionPane.showMessageDialog(null, cliente);
+            
+    }
+}
