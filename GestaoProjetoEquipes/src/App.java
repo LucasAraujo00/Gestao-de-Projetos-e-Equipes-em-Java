c24972c9b97fee1a37c41d3b9ca0b0b409577557
lucas araujo <70071400+lucasaraujoXD@users.noreply.github.com>
2026-04-16 17:48:19 -0300

    tela login

--- GestaoProjetoEquipes/src/App.java
+++ GestaoProjetoEquipes/src/App.java
@@ -1,54 +1,39 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import models.Usuario;
+import services.UsuarioService;
 
 public class App {
-   
 
     public static void main(String[] args) {
-        
-        String primeiroNome = "Lucas";
-        String ultimoNome = "Araujo";
-        
-        String nomeCompleto = primeiroNome + " " + ultimoNome;
-        
-        System.out.println(primeiroNome);
-        System.out.println(ultimoNome);
-        System.out.println(nomeCompleto);
-        
-        int numero1 = 2;
-        int numero2 = 3;
-        
-        
-        int resultadoSoma = numero1 + numero2;
-        
-        System.out.println(resultadoSoma);
-        
-        String numero3 = "2";
-        String numero4 = "3";
-        
-        String resultadoJuncao = numero3 + numero4;
-        
-        System.out.println(resultadoJuncao);
-        
-        double a = 1;
-        double b = 2;
-        
-        double soma = a + b;
-        double subtracao = a - b;
-        double multiplicacao = a * b;
-        double divisao = a / b;
-        
-        System.out.println("soma: " + soma);
-        System.out.println("subtração: " + subtracao);
-        System.out.println("multiplicação: " + multiplicacao);
-        System.out.println("divisão: " + divisao);
-        
-        String firstName = JOptionPane.showInputDialog("Digite seu primeiro nome: ");
-        String lastName = JOptionPane.showInputDialog("Digite seu último nome: ");
-        String idade = JOptionPane.showInputDialog("Qual é a sua idade? : ");
-        
-        String cliente = firstName + " " + lastName + " com idade de " + idade + " anos";
-        
-        JOptionPane.showMessageDialog(null, cliente);
-            
+
+        UsuarioService service = new UsuarioService();
+
+        // usuário 1
+        Usuario admin = new Usuario();
+        admin.setLogin("admin");
+        admin.setSenha("123");
+        admin.setNome("Lukas");
+
+        // usuário 2
+        Usuario user = new Usuario();
+        user.setLogin("joao");
+        user.setSenha("123");
+        user.setNome("João");
+
+        service.cadastrar(admin);
+        service.cadastrar(user);
+
+        String login = JOptionPane.showInputDialog("Login:");
+        String senha = JOptionPane.showInputDialog("Senha:");
+
+        Usuario logado = service.login(login, senha);
+
+        if (logado != null) {
+            JOptionPane.showMessageDialog(null,
+                    "Bem-vindo " + logado.getNome());
+        } else {
+            JOptionPane.showMessageDialog(null,
+                    "Login inválido!");
+        }
     }
 }
---
+++ GestaoProjetoEquipes/src/services/UsuarioService.java
@@ -0,0 +1,26 @@
+package services;
+
+import java.util.ArrayList;
+import java.util.List;
+import models.Usuario;
+
+public class UsuarioService {
+
+    private List<Usuario> usuarios = new ArrayList<>();
+
+    public void cadastrar(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    public Usuario login(String login, String senha) {
+
+        for (Usuario u : usuarios) {
+            if (u.getLogin().equals(login) &&
+                u.getSenha().equals(senha)) {
+                return u;
+            }
+        }
+
+        return null;
+    }
+}
